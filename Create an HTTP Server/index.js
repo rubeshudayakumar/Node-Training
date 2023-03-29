038e6db0079f2fa834a3d531970d1c212ef1ae00
rubeshudayakumar <rubesh.udayakumar@cdw.com>
2023-03-29 14:32:58 +0530

    commit

--- Create an HTTP Server/index.js
+++ Create an HTTP Server/index.js
@@ -2,8 +2,16 @@ const http = require("http");
 const FileIO = require("./modules/FileIO");
 
 http.createServer(async (req,res,err) => {
+    let response;
     // loading all the color palettes from the color_palette.json file
-    let response = await FileIO.loadJSONFile("./data/color_ palette.json");
+    try{
+        response = await FileIO.loadJSONFile("./data/color_ palette.json");
+    }
+    catch(err){
+        res.write("some error occured!");
+        res.end();
+        return;
+    }
     // converting it to the json object
     let color_palette =  JSON.parse(response);
     let colorsLength = color_palette.length;
@@ -13,10 +21,30 @@ http.createServer(async (req,res,err) => {
     while(i++<=5){
        randomFiveColors.push(color_palette[Math.floor(Math.random() * colorsLength)+1]);
     }
+    if(randomFiveColors.length<5){
+        res.write("error in generating the random colors!");
+        res.end();
+        return;
+    }
     // storing the 5 colors into the randomized_color_palette.json file
-    FileIO.storeJSONFile(JSON.stringify(randomFiveColors));
+    try{
+        FileIO.storeJSONFile(JSON.stringify(randomFiveColors));
+    }
+    catch{
+        res.write("some error occured!");
+        res.end();
+        return;
+    }
     // loading from the randomized_color_palette.json and printing it to the console
-    let fiveColorsFromFIle = JSON.parse(await FileIO.loadJSONFile("./data/randomized_color_ palette.json"));
+    let fiveColorsFromFIle;
+    try{
+       fiveColorsFromFIle = JSON.parse(await FileIO.loadJSONFile("./data/randomized_color_ palette.json"));
+    }
+    catch(err){
+        res.write("some error occured!");
+        res.end();
+        return;
+    }
     res.write(JSON.stringify(fiveColorsFromFIle));
     res.end();
 }).listen(4000);
--- Create an HTTP Server/modules/FileIO.js
+++ Create an HTTP Server/modules/FileIO.js
@@ -12,5 +12,11 @@ exports.loadJSONFile = (fileUrl) => {
 }
 
 exports.storeJSONFile = (data) => {
-    fs.writeFileSync("./data/randomized_color_ palette.json",data,"utf-8");
+    fs.writeFileSync("./data/randomized_color_ palette.json",data,"utf-8",(err) => {
+        if(err){
+            console.log(err);
+        }else{
+            console.log("data stored");
+        }
+    });
 }
