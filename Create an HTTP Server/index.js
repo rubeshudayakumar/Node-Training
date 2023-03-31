3d3ca4890c47c6aa6a545581b75952b7dd82d7b6
rubeshudayakumar <rubesh.udayakumar@cdw.com>
2023-03-31 11:53:16 +0530

    commit

--- Create an HTTP Server/index.js
+++ Create an HTTP Server/index.js
@@ -1,11 +1,14 @@
 const http = require("http");
 const FileIO = require("./modules/FileIO");
 
-http.createServer(async (req,res,err) => {
+http.createServer((req,res,err) => {
     let response;
     // loading all the color palettes from the color_palette.json file
     try{
-        response = await FileIO.loadJSONFile("./data/color_ palette.json");
+        response = FileIO.loadJSONFile("./data/color_ palette.json");
+        if(response==undefined || response==null){
+            throw "Some error";
+        }
     }
     catch(err){
         res.write("some error occured!");
@@ -38,7 +41,7 @@ http.createServer(async (req,res,err) => {
     // loading from the randomized_color_palette.json and printing it to the console
     let fiveColorsFromFIle;
     try{
-       fiveColorsFromFIle = JSON.parse(await FileIO.loadJSONFile("./data/randomized_color_ palette.json"));
+       fiveColorsFromFIle = JSON.parse(FileIO.loadJSONFile("./data/randomized_color_ palette.json"));
     }
     catch(err){
         res.write("some error occured!");
