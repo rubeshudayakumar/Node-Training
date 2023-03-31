7b073a9a815109e953c8cb498e26b09fbb81c5c6
rubeshudayakumar <rubesh.udayakumar@cdw.com>
2023-03-31 15:31:10 +0530

    commit

--- Randomize the color palette/index.js
+++ Randomize the color palette/index.js
@@ -1,39 +1,76 @@
 const fileIo = require("./modules/file-io");
 
 const displayRandomColors =  () => {
+   let response = "";
    const filePaths = [
-      "./data/color_ palette.json",
-      "./data/randomized_color_ palette.json",
+      "./data/color_palette.json",
+      "./data/randomized_color_palette.json",
    ];
-   // loading all the color palettes from the color_palette.json file
-   const response = "";
-   try{
-      response = fileIo.loadFile(filePaths[0]);
-   }
-   catch{
-      console.log("error in opening the file");
-      return;
-   }
-   
-   if(response==""){
-      console.log("file is empty");
-      return;
-   }
-   console.log(response)
-   // converting it to the json object
-   const colorPalette =  JSON.parse(response);
-   const colorsLength = colorPalette.length;
-   // selecting the random five colors from the json 
    let randomFiveColors = [];
-   let i = 1;
-   while(i++<=5){
-      randomFiveColors.push(colorPalette[Math.floor(Math.random() * colorsLength)+1]);
+
+   // loading all the color palettes from the color_palette.json file
+   const readFile = () =>  {
+      try{
+         response = fileIo.loadFile(filePaths[0]);
+         if(response.length==0){
+            throw new Error("file is empty");
+         }
+         return true;
+      }
+      catch(err){
+         console.log(err.message);
+         return false;
+      }
    }
-   // storing the 5 colors into the randomized_color_palette.json file
-   fileIo.storeFile(filePaths[1],JSON.stringify(randomFiveColors));
-   // loading from the randomized_color_palette.json and printing it to the console
-   const fiveColorsFromFIle = JSON.parse(fileIo.loadFile(filePaths[1]));
-   console.log(fiveColorsFromFIle);
+   const isRead = readFile();
+   if(!isRead) return;
+   
+   const generateRandomColors = () => {
+      try{
+         let insertedRandoms = [];         
+          // converting it to the json object
+         const colorPalette =  JSON.parse(response);
+         if(colorPalette.length<5){
+            throw new Error("there are not enough available colors");
+         }
+         const colorsLength = colorPalette.length;
+         // selecting the random five colors from the json 
+         let i = 1;
+         while(i<=5){
+            const randomNumber = Math.floor(Math.random() * colorsLength);
+            if(insertedRandoms.indexOf(randomNumber)==-1){
+               randomFiveColors.push(colorPalette[randomNumber]);
+               insertedRandoms.push(randomNumber);
+               i++;
+               continue;
+            }
+         }
+         if(randomFiveColors.length<5){
+            throw new Error("Couldn't able to get random 5 colors");
+         }
+         return true;
+      }
+      catch(err){
+         console.log(err.message);
+         return false;
+      }
+   }
+   const isGotFiveColors = generateRandomColors();
+   if(!isGotFiveColors) return;
+   
+   const storeAndDisplayColors = () => {
+      try{
+         // storing the 5 colors into the randomized_color_palette.json file
+         fileIo.storeFile(filePaths[1],JSON.stringify(randomFiveColors));
+         // loading from the randomized_color_palette.json and printing it to the console
+         const fiveColorsFromFIle = JSON.parse(fileIo.loadFile(filePaths[1]));
+         console.log(fiveColorsFromFIle);
+      }
+      catch(err){
+         console.log(err.message);
+      }  
+   }
+   storeAndDisplayColors();
 }
 
 displayRandomColors();
