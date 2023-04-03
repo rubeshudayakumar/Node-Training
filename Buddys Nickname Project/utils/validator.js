5a6cacb32cc3fd6cb6a40316709559dcddd2ef65
rubeshudayakumar <rubesh.udayakumar@cdw.com>
2023-04-03 18:55:10 +0530

    commit

--- Buddys Nickname Project/utils/validator.js
+++ Buddys Nickname Project/utils/validator.js
@@ -1,4 +1,7 @@
 const validator = (buddy) => {
+    if(buddy.employeeId == undefined || buddy.realName==undefined || buddy.nickName==undefined || buddy.dob==undefined || buddy.hobbies==undefined){
+        return false;
+    }
     if((/^[0-9]{1,30}$/).test(buddy.employeeId)==false || (/^[a-zA-Z ]{1,30}$/).test(buddy.realName)==false || (/^[a-zA-Z ]{1,30}$/).test(buddy.nickName)==false || 
     (/^[0-9]{2}-[0-9]{2}-[0-9]{4}$/).test(buddy.dob)==false || buddy.hobbies==undefined || buddy.length<=0){
         return false;
