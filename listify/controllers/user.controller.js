4ba4b74f06d8c754eb35f0a521fce28a03f457b6
rubeshudayakumar <rubesh.udayakumar@cdw.com>
2023-04-04 14:25:08 +0530

    commit

--- listify/controllers/user.controller.js
+++ listify/controllers/user.controller.js
@@ -1,5 +1,6 @@
 const warnLogger = require("../utils/logger").warnLogger;
 const userIdAndPasswordValidator = require("../utils/validator").userIdAndPasswordValidator;
+const userService = require("../services/user.services");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
@@ -8,8 +9,21 @@ const userLogin = (req,res) => {
 }
 
 const userRegister = async (req,res) => {
-  
+    try{
+        if(!userIdAndPasswordValidator(req.body)){
+            throw new Error("username and passwords are invalid format");
+        }
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hashedPassword = await bcrypt.hash(req.body.password,salt);
+        const userData = await userService.readUser(req,res);
+        userData.push({userName: req.body.userName,password: hashedPassword});
+        userService.writeUser(req,res,userData);
         res.status(200).send({"message" : "registration successful"});
+    }
+    catch(err){
+        warnLogger.warn(`${err.status || 403} - ${err} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+        res.status(403).send({"message" : "username should contain alphabets or under score and password should be atleast 8 characters"});
+    }
 }
 
 module.exports = {
--- listify/services/user.services.js
+++ listify/services/user.services.js
@@ -1,1 +1,34 @@
 const errorLogger = require("../utils/logger").errorLogger;
+const fs = require("fs");
+
+const readUser = (req,res) => {
+    return new Promise((resolve,reject) => {
+        fs.readFile("./data/user_data.json","UTF-8",(err,data) => {
+            if(err){
+                errorLogger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+                return res.status(500).send({message: "error in reading the file"});
+            }else{
+                resolve(JSON.parse(data));
+            }
+        });
+    });
+}
+
+const writeUser = (req,res,data) => {
+    return new Promise((resolve,reject) => {
+        fs.writeFile("./data/user_data.json",JSON.stringify(data),"UTF-8",(err,data) => {
+            if(err){
+                errorLogger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+                return res.status(500).send({message: "error in reading the file"});
+            }
+            else{
+                resolve(data);
+            }
+        });
+    });
+}
+
+module.exports = {
+    readUser,
+    writeUser,
+}
