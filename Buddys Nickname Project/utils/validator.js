ff794b04775183f0ec12fee53678a6f2e140931e
rubeshudayakumar <rubesh.udayakumar@cdw.com>
2023-04-05 19:54:26 +0530

    commit

--- Buddys Nickname Project/utils/validator.js
+++ Buddys Nickname Project/utils/validator.js
@@ -1,10 +1,27 @@
+const validationObjects = [
+    {
+        keyName : "employeeId",
+        regEx: /^[0-9]{1,30}$/,
+    },
+    {
+        keyName : "realName",
+        regEx: /^[a-zA-Z ]{1,30}$/,
+    },
+    {
+        keyName : "nickName",
+        regEx : /^[a-zA-Z ]{1,30}$/,
+    },
+    {
+        keyName : "dob",
+        regEx : /^[0-9]{2}-[0-9]{2}-[0-9]{4}$/,
+    },
+];
+
 const validator = (buddy) => {
-    if(buddy.employeeId == undefined || buddy.realName==undefined || buddy.nickName==undefined || buddy.dob==undefined || buddy.hobbies==undefined){
-        return false;
-    }
-    if((/^[0-9]{1,30}$/).test(buddy.employeeId)==false || (/^[a-zA-Z ]{1,30}$/).test(buddy.realName)==false || (/^[a-zA-Z ]{1,30}$/).test(buddy.nickName)==false || 
-    (/^[0-9]{2}-[0-9]{2}-[0-9]{4}$/).test(buddy.dob)==false || buddy.hobbies==undefined || buddy.length<=0){
-        return false;
+    for(var i=0;i<validationObjects.length;i++){
+        if(!(validationObjects[i].keyName in buddy) || (validationObjects[i].regEx).test(buddy[validationObjects[i].keyName])==false){
+            return false;
+        }
     }
     return true;
 }
