482903f4b0b457a3cde53705a19e601a885ff8cd
rubeshudayakumar <rubesh.udayakumar@cdw.com>
2023-04-05 19:27:46 +0530

    commit

--- Enhance Buddys Nickname Project/services/FileRead.services.js
+++ Enhance Buddys Nickname Project/services/FileRead.services.js
@@ -1,9 +1,11 @@
 const fs = require("fs");
 const errorLogger = require("../utils/Logger").errorLogger;
 
+const path = "./data/cdw_ace23_buddies.json";
+
 const fileRead = (req,res) => {
     return new Promise((resolve,reject) => {
-        fs.readFile("./data/cdw_ace23_buddies.json","utf-8",(err,data) => {
+        fs.readFile(path,"utf-8",(err,data) => {
             if(err){
                 errorLogger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
                 reject(err);
--- Enhance Buddys Nickname Project/services/fileWrite.services.js
+++ Enhance Buddys Nickname Project/services/fileWrite.services.js
@@ -1,9 +1,11 @@
 const fs = require("fs");
 const errorLogger = require("../utils/Logger").errorLogger;
 
+const path = "./data/cdw_ace23_buddies.json";
+
 const fileWrite = (req,res,data) => {   
     return new Promise((resolve,reject) => {
-        fs.writeFile("./data/cdw_ace23_buddies.json",JSON.stringify(data),"utf-8",(err,data) => {
+        fs.writeFile(path,JSON.stringify(data),"utf-8",(err,data) => {
             if(err){
                 errorLogger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
                 reject(err);
