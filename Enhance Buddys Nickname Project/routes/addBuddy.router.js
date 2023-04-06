d5580322e398bf12180759af15f44f29f3bd0e17
rubeshudayakumar <rubesh.udayakumar@cdw.com>
2023-04-06 12:14:55 +0530

    commit

--- Enhance Buddys Nickname Project/routes/addBuddy.router.js
+++
@@ -1,8 +0,0 @@
-const buddyWriteController = require("../controllers/BuddyWrite.controller");
-const router = require("express").Router();
-
-router.post("/",buddyWriteController.addBuddy);
-
-module.exports = {
-    router
-};
