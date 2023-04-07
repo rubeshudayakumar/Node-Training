8dbbd67a3ad0bf022dab0b35bc834b0cd8d92333
rubeshudayakumar <rubesh.udayakumar@cdw.com>
2023-04-07 10:20:42 +0530

    commit

--- listify-folder-structure/utils/validator.js
+++ listify-folder-structure/utils/validator.js
@@ -1,7 +1,4 @@
 const userIdAndPasswordValidator = (user) => {
-    if((/^[a-zA-Z_]{1,30}$/).test(user.userName)==false || (/^[a-zA-Z0-9\W]{8,}$/).test(user.password)==false || user.userName==undefined || user.password==undefined){
-        return false;
-    }
     return true;
 }
 
